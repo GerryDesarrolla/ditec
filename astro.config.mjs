7c4716c2f1fd3ba2404638b65abcf995f6b9b84e
GerryDesarrolla <gerrydesarrolla@gmail.com>
2024-02-18 16:05:02 -0600

    Agregando y optimizando codigo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://ditecs.solutions',
+  integrations: [tailwind(), robotsTxt(), sitemap()]
 });
