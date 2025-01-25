b69f973e2243e2dcff1671daa1cf91a3527ae155
Gerry <luisgerardo@flexbpo.com>
2025-01-24 23:51:45 -0600

    Cambio de marca por DISAK

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ditecs.solutions',
+  site: 'https://disaksolutions.com',
   integrations: [tailwind(), robotsTxt(), sitemap()]
 });
